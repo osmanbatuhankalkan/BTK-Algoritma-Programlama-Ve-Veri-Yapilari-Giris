09dd84a75567bf03a699360ff9a0762ea8975ad7
Osman Batuhan Kalkan <119897554+osmanbatuhankalkann@users.noreply.github.com>
2023-04-26 21:33:50 +0300

    Add files via upload

---
+++ Karakter_Sabitleri-BTK/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Program
+{
+    class Karakter_Sabitleri
+    {
+        static void Main(string[] args)
+        {
+            //BasitDegiskenOrnegi();
+            string ifade = "\nBTK \nAkademi \nProgramlama \nOgreniyorum."; //alt alta
+            string ifade2 = "\tBTK \tAkademi \tProgramlama \tOgreniyorum."; //bosluk
+            string ifade3 = "\aBTK \tAkademi \tProgramlama \tOgreniyorum."; //alrm
+            System.Console.WriteLine(ifade);
+            System.Console.WriteLine(ifade2);
+            System.Console.WriteLine(ifade3);
+
+            Console.ReadKey();
+        }
+        private static void BasitDegiskenOrnegi()
+        {
+            int sayi = 3;
+            System.Console.WriteLine(sayi);
+            System.Console.WriteLine(sayi + 2); //3+2 = 5
+            System.Console.WriteLine(sayi * sayi); /*Karesi*/ 
+            System.Console.WriteLine(sayi -5 );
+        }
+    }
+}
