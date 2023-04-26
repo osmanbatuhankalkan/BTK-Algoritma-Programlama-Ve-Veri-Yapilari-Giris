6d396569074b3b0a46353e53563281e9f75cf667
Osman Batuhan Kalkan <119897554+osmanbatuhankalkann@users.noreply.github.com>
2023-04-26 21:07:41 +0300

    Add files via upload

---
+++ Degiskenler_BTK/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Program
+{   
+    public class Program
+    {
+        static void Main(string[] args)
+    {   
+        //TİP
+        int sayi1;
+        double pi = 3.14;
+        char secim = 'e';
+        string isim = "zafer";
+        bool dogruMu = false;
+
+        string isim2 = "Ahmet";
+        System.Console.WriteLine("Merhaba " + isim2 + ". ");//cw+tab ile kısa yoldan console.writeline yazdırırsın
+        System.Console.WriteLine("Merhaba Sayın..." + isim2 + ". ");
+        Console.ReadKey();
+        // fark ettiysek isim adlı değişken tanımladık 2 kere kullandık
+
+        int sayiDeneme = 5;
+        System.Console.WriteLine(sayiDeneme);
+        System.Console.WriteLine(sayiDeneme + 2);
+        System.Console.WriteLine(sayiDeneme * sayiDeneme);
+        System.Console.WriteLine(sayiDeneme - 5);
+    }
+
+    }
+    
+}
