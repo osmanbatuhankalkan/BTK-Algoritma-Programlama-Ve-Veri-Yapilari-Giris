ecc648bbfb4334aa59d6805097457acdaa5c2997
Osman Batuhan Kalkan <119897554+osmanbatuhankalkann@users.noreply.github.com>
2023-04-29 23:28:08 +0300

    Add files via upload

---
+++ Operatorler-BTK/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Operatorler
+{
+    class Program
+    {
+        static void Main(string[] args)
+    {
+        int A=20, B=10;
+
+        //Aritmetik Operatörler
+        Console.WriteLine(A+B);
+        Console.WriteLine(A-B);
+        Console.WriteLine(A*B);
+        Console.WriteLine(A/B);
+        Console.WriteLine(A%B);
+        Console.ReadKey();
+
+        //İlişkisel Operatörler
+        System.Console.WriteLine(A>B);
+        System.Console.WriteLine(A<B);
+        System.Console.WriteLine(A>=B);
+        System.Console.WriteLine(A<=B);
+        System.Console.WriteLine(A==B);
+        System.Console.WriteLine(A!=B);
+        Console.ReadKey();
+
+        System.Console.WriteLine(A>B && A>5); //VE
+        System.Console.WriteLine(A>B && A<5);  //VE
+        System.Console.WriteLine(!(A>B && A<5)); //VE
+        System.Console.WriteLine(A<B || B>5); //VEYA
+        Console.ReadKey();
+            }
+
+    }
+    
+}
