71bfa9d4f1dc6a868fb58d1d66200f3657be94aa
Osman Batuhan Kalkan <119897554+osmanbatuhankalkann@users.noreply.github.com>
2023-05-01 14:41:24 +0300

    Add files via upload

---
+++ Donguler-1-While-BTK/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Donguler_1_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Anlaşılması en kolay olan while ile başlıyoruz while = iken demek
+            int i =0;
+            int sayac = 0; //Döngü kaç kere dönece bakalım
+            while (i<10)
+            {   
+                sayac++;// ifade sonsuza doğru saymaya başlatır İpucu:durudurmak için ctrl+c
+                System.Console.WriteLine(sayac);
+                i++;//döngü ilerlesin diye. Arttırıma gitmezsek sonsuza gider yukardaki tanımlama bir işe yaramaz bu sayacın artmasıda lazım
+            }
+            System.Console.WriteLine("Döngü Sonu");
+            Console.ReadKey();
+        }
+    }
+}
