41faef9f8cb6d3714fc8642bd48a6baee1615217
Osman Batuhan Kalkan <119897554+osmanbatuhankalkan@users.noreply.github.com>
2023-05-11 01:09:05 +0300

    Add files via upload

---
+++ Çok-Boyutlu-Diziler-BTK-1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BTKCokboyutluDiziler
+{   
+    //Boyut bilgisini dışarıdan alacağız ve random oluşturacağımız bir sistem yapıcaz aynı zamanda bu random değerlerinde karesini alıcaz
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Dizi boyutunu giriniz:  ");
+            int boyut = Convert.ToInt32(Console.ReadLine());
+
+            int[] sayilar = new int[boyut];
+            var r = new Random();
+
+            for(int i=0;i <sayilar.Length ;i++)
+            {
+                sayilar[i] = r.Next(1,10);//kavramı rastgele ifade üretir
+            }
+
+            foreach (var s in sayilar)//foreach koleksiyonlar üzerinde gezinmek için kullanılır (koleksiyonlar sadece diziler değildir)
+            {
+                System.Console.WriteLine($"{s,5} {s*s,5}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
