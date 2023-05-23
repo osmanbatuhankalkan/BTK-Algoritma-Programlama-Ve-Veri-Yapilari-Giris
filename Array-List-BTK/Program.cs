cdbab2abb0f3a326bdf3c39ab8f3356afd29983a
Osman Batuhan Kalkan <119897554+osmanbatuhankalkan@users.noreply.github.com>
2023-05-23 23:48:48 +0300

    Add files via upload

---
+++ Array-List-BTK/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace Array_List_BTK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //Tanımlama
+           var arrayList = new ArrayList(){
+            10,"BTK Akademi",true, 'e'};
+            /*
+           //Ekleme
+           arrayList.Add(10);//boxing
+           arrayList.Add("BTK Akademi");
+           arrayList.Add(true);
+           arrayList.Add('e');
+            */
+           //Dolaşma
+           foreach (var e in arrayList)
+           {
+            System.Console.WriteLine($"{e}");
+           }
+           
+           int[] sayilar = new int[] {23,44,55};
+           arrayList.AddRange(sayilar);
+
+           //Dolaşma
+           System.Console.WriteLine();
+           foreach (var e in arrayList)
+           {
+            System.Console.WriteLine($"{e}");
+           }
+           System.Console.WriteLine();
+
+           //Elemana Erişme
+           System.Console.WriteLine(arrayList[4]);
+
+           //Elemana erişme - atama
+           int x = (int)arrayList[0];//kutudan çıkarma
+           System.Console.WriteLine(x+10);
+
+           //Eleman Sİlme
+           //arrayList.Remove(10);
+           //arrayList.RemoveAt(1);
+           arrayList.RemoveRange(3,3);
+           //Dolaşma
+           System.Console.WriteLine();
+           foreach(var e in arrayList)
+           {
+            System.Console.WriteLine($"{e}");
+           }
+
+           Console.ReadKey();
+
+        }
+    }
+}
