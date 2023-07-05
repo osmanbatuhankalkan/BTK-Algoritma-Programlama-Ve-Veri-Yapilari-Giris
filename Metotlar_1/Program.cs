e82b6a0b4ef121d6fbb9a273563992a824cf92f8
Osman Batuhan Kalkan <119897554+osmanbatuhankalkan@users.noreply.github.com>
2023-07-05 23:41:53 +0300

    Add files via upload

---
+++ Metotlar_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Metotlar
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int buyuk = Karsilastir(3,5);
+            System.Console.WriteLine(buyuk);
+            
+            var x = KareAl(3);
+            double y = KareAl(x);
+
+            System.Console.WriteLine(x);
+            System.Console.WriteLine(y);
+
+            Console.ReadKey();
+
+
+        }
+        public static int Karsilastir(int A, int B)
+        {
+            if(A>B)
+            {
+                return A;
+            }
+            else
+            {
+                return B;
+            }
+        }
+        static double KareAl(double sayi)
+        {
+            double kare = sayi*sayi;
+            return kare;
+        }
+    }
+}
